85ee23cf7ac4e33328763832dca7fff398b75fc8
odenmn <3461727951@qq.com>
2025-04-29 19:43:26 +0800

    like实体类

---
+++ XuJunxi_CloudMusic/src/main/java/com/xjx/example/entity/Like.java
@@ -0,0 +1,43 @@
+package com.xjx.example.entity;
+
+import java.util.Date;
+
+public class Like {
+    private Integer id;
+    private int userId;
+    private int songId;
+    private Date createdAt;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
