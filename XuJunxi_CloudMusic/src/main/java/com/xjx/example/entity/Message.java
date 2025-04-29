6d49e2dd63cf0167b71dca42ea14207633cbee7d
odenmn <3461727951@qq.com>
2025-04-29 19:56:52 +0800

    消息的实体类

---
+++ XuJunxi_CloudMusic/src/main/java/com/xjx/example/entity/Message.java
@@ -0,0 +1,52 @@
+package com.xjx.example.entity;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Message {
+    private Integer id;
+    private Integer userId;
+    private String content;
+    private boolean isRead;
+    private LocalDateTime createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+}
